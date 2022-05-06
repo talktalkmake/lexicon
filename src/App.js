04ea068f0af6de6f3f9fed0d2614bdc7c07e018a
Tom Hart <kitsonian@gmail.com>
2022-05-06 16:51:42 -0400

    Adding TailwindCSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import './css/main.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Lexicon</h1>
+      <h1 className='text-5xl'>Lexicon</h1>
+      {/* Make a collection of words */}
+      {/* Add word to collection */}
+      {/* Remove word from collection */}
+      {/* Look up a word (and its synonyms, antonyms, etc.) */}
     </div>
   );
 }
