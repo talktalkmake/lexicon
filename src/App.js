2b1405e2052bd869f07084890e58e01b4d5e534b
Tom Hart <kitsonian@gmail.com>
2022-05-06 17:59:15 -0400

    Added useReducer and useState to manage adding a words to state and displaying it in a table

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
+import React, { useReducer, useState } from 'react';
 import './css/main.css';
 
+const ACTION = {
+  'ADDWORD': 'addWord'
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'addWord':
+      return [...state, action.word]
+      break;
+
+    default:
+      return state;
+  }
+}
+
 function App() {
+
+  const [words, dispatch] = useReducer(reducer, [])
+  const [word, setWord] = useState('')
+
   return (
     <div className="App">
       <h1 className='text-5xl'>Lexicon</h1>
+      <form onSubmit={e => {
+        e.preventDefault();
+        dispatch({ type: 'addWord', word });
+        setWord('');
+      }}>
+        <input type='text' value={word} onChange={e => setWord(e.target.value)} />
+        <button type='submit' className='bg-blue-500 rounded-lg text-white px-4 py-2'>Add new word to collection</button>
+      </form>
+      <ListWords words={words}></ListWords>
+      {/* <pre>{JSON.stringify(words, null, 2)}</pre> */}
       {/* Make a collection of words */}
       {/* Add word to collection */}
       {/* Remove word from collection */}
@@ -28,3 +58,22 @@ function getter() {
 }
 
 export default App;
+
+function ListWords({ words }) {
+  return (words.length === 0)
+    ? <p>There are no words</p>
+    : <>
+      <table>
+        <tbody>
+          {words.map((word, i) => {
+            return (
+              <tr key={`${word}-${i}`}>
+                <td>{word}</td>
+                <td><button className='bg-red-600 text-white px-4 py-2 rounded-full'>remove</button></td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </>
+}
