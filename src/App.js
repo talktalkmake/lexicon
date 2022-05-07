a79d7e54a9531b45aae75fbc98b96746930665bd
Tom Hart <kitsonian@gmail.com>
2022-05-07 12:13:52 -0400

    Finessing layout

--- src/App.js
+++ src/App.js
@@ -45,55 +45,74 @@ function App() {
       .catch(err => console.error(err));
   }
 
+  function ShowDefinitionList(lexicon) {
+    return (<>
+      {lexicon.map((wordInLexicon, i) =>
+        <section
+          key={`${wordInLexicon.word}-${i}`}
+          className='shadow bg-white rounded p-10 mt-10'>
+          <article className='relative'>
+            <button
+              className='bg-red-600 text-white px-4 py-2 rounded-full absolute top-0 right-0'
+              onClick={() => dispatch({ type: ACTION.REMOVEWORD, wordInLexicon })}>
+              remove
+            </button>
+            <h3 className='text-2xl font-semibold uppercase tracking-widest'>{wordInLexicon.word}</h3>
+            {wordInLexicon.definitions.map(definition =>
+              <dl key={definition.definition}>
+                <dt className='italic font-serif pt-5'>{definition.partOfSpeech}</dt>
+                <dd className='text-xl'>{definition.definition}</dd>
+              </dl>)}
+          </article>
+        </section >
+      )}
+    </>
+    );
+  }
+
   function ShowLexicon({ lexicon }) {
     return (lexicon.length === 0)
-      ? <p>There are no words</p>
-      : <>
-        {lexicon.map((wordInLexicon, i) =>
-          <section
-            key={`${wordInLexicon.word}-${i}`}
-            className='shadow bg-white rounded p-10 mt-10'>
-            <article className='relative'>
-              <button
-                className='bg-red-600 text-white px-4 py-2 rounded-full absolute top-0 right-0'
-                onClick={() => dispatch({ type: ACTION.REMOVEWORD, wordInLexicon })}>
-                remove
-              </button>
-              <h3 className='text-2xl font-semibold uppercase tracking-widest'>{wordInLexicon.word}</h3>
-              {wordInLexicon.definitions.map(definition =>
-                <dl key={definition.definition}>
-                  <dt className='italic font-serif pt-5'>{definition.partOfSpeech}</dt>
-                  <dd className='text-xl'>{definition.definition}</dd>
-                </dl>)}
-            </article>
-          </section >
-        )}
+      ? <>
+        <p className='mt-10 text-3xl'>Your lexicon has no words</p>
+        <p className='mt-2 text-xl'>Search for a word at the top of the page.</p>
       </>
+      : ShowDefinitionList(lexicon)
   }
 
   return (
-    <div className="container p-4">
-      <h1 className='text-5xl'>Lexicon</h1>
-      <form onSubmit={e => {
-        e.preventDefault();
-        // dispatch({ type: ACTION.ADDWORD, word });
-        getWordFromAPI();
-        setWord('');
-      }}>
-        <input type='text' value={word} onChange={e => setWord(e.target.value)} />
-        <button className='bg-blue-900 rounded-lg text-white px-4 py-2'>Search for word</button>
-        {/* <button type='submit' className='bg-blue-500 rounded-lg text-white px-4 py-2'>Add new word to collection</button> */}
-      </form>
-      {definition
-        && <section>
-          {definition.definitions.map(definition => <dl key={definition.definition}><dd>{definition.partOfSpeech}</dd><dt>{definition.definition}</dt></dl>)}
+    <div>
+      <header className='bg-zinc-900 flex align-center justify-between px-5 py-2'>
+        <h1 className='text-2xl text-white'>Lexicon</h1>
+        <form onSubmit={e => {
+          e.preventDefault();
+          // dispatch({ type: ACTION.ADDWORD, word });
+          getWordFromAPI();
+          setWord('');
+        }}>
+          <input
+            className='form-control px-3 py-2 rounded-l-lg'
+            type='text'
+            value={word}
+            onChange={e => setWord(e.target.value)} />
           <button
-            onClick={() => dispatch({ type: ACTION.ADDWORD, definition })}
-            className='bg-green-300 px-4 py-2 hover:bg-green-400'>
-            Add {definition.word} to Lexicon
+            className='bg-pink-300 rounded-r-lg text-red-900 px-4 py-2'>
+            Search for word
           </button>
-        </section>}
-      <ShowLexicon lexicon={lexicon}></ShowLexicon>
+          {/* <button type='submit' className='bg-blue-500 rounded-lg text-white px-4 py-2'>Add new word to collection</button> */}
+        </form>
+      </header>
+      <div className='container p-10'>
+        {definition
+          && <section>
+            {definition.definitions.map(definition => <dl key={definition.definition}><dd>{definition.partOfSpeech}</dd><dt>{definition.definition}</dt></dl>)}
+            <button
+              onClick={() => dispatch({ type: ACTION.ADDWORD, definition })}
+              className='bg-green-300 px-4 py-2 hover:bg-green-400'>
+              Add {definition.word} to Lexicon
+            </button>
+          </section>}
+        <ShowLexicon lexicon={lexicon}></ShowLexicon>
+      </div>
       {/* Look up a word (and its synonyms, antonyms, etc.) */}
     </div>
   );
