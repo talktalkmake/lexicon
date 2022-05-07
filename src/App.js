d855bf9e0cf64de72e6d3b33305c39f207ef3b1d
Tom Hart <kitsonian@gmail.com>
2022-05-07 10:27:48 -0400

    Adding words API lookup and adding objects to state, rather than strings

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const ACTION = {
 const reducer = (state, action) => {
   switch (action.type) {
     case ACTION.ADDWORD:
-      return [...state, action.word];
+      return [...state, action.definition];
       break;
 
     case ACTION.REMOVEWORD:
@@ -25,6 +25,22 @@ function App() {
 
   const [words, dispatch] = useReducer(reducer, [])
   const [word, setWord] = useState('')
+  const [definition, setDefinition] = useState('')
+
+  function getWordFromAPI() {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com',
+        'X-RapidAPI-Key': '06678c9f50msh8e8dc3e7442f9f3p14404djsne386b9f689a7'
+      }
+    }
+
+    fetch(`https://wordsapiv1.p.rapidapi.com/words/${word}/definitions`, options)
+      .then(response => response.json())
+      .then(response => setDefinition(response))
+      .catch(err => console.error(err));
+  }
 
   function ListWords({ words }) {
     return (words.length === 0)
@@ -32,14 +48,15 @@ function App() {
       : <>
         <table className='table-auto shadow w-full'>
           <tbody>
-            {words.map((word, i) => {
-              return (
-                <tr key={`${word}-${i}`}>
-                  <td className='px-4 py-2'>{word}</td>
-                  <td className='px-4 py-2'><button onClick={() => dispatch({ type: ACTION.REMOVEWORD, word }} className='bg-red-600 text-white px-4 py-2 rounded-full'>remove</button></td>
-                </tr>
-              )
-            })}
+            {words.map((word, i) =>
+              <tr key={`${word.word}-${i}`}>
+                <td className='px-4 py-2'>
+                  {word.word}
+                  {word.definitions.map(definition => <dl><dt>{definition.partOfSpeech}</dt><dd>{definition.definition}</dd></dl>)}
+                </td>
+                <td className='px-4 py-2'><button onClick={() => dispatch({ type: ACTION.REMOVEWORD, word })} className='bg-red-600 text-white px-4 py-2 rounded-full'>remove</button></td>
+              </tr>
+            )}
           </tbody>
         </table>
       </>
@@ -50,35 +67,29 @@ function App() {
       <h1 className='text-5xl'>Lexicon</h1>
       <form onSubmit={e => {
         e.preventDefault();
-        dispatch({ type: ACTION.ADDWORD, word });
+        // dispatch({ type: ACTION.ADDWORD, word });
+        getWordFromAPI();
         setWord('');
       }}>
         <input type='text' value={word} onChange={e => setWord(e.target.value)} />
-        <button type='submit' className='bg-blue-500 rounded-lg text-white px-4 py-2'>Add new word to collection</button>
+        <button className='bg-blue-900 rounded-lg text-white px-4 py-2'>Search for word</button>
+        {/* <button type='submit' className='bg-blue-500 rounded-lg text-white px-4 py-2'>Add new word to collection</button> */}
       </form>
+      {definition
+        && <section>
+          {definition.definitions.map(definition => <dl><dd>{definition.partOfSpeech}</dd><dt>{definition.definition}</dt></dl>)}
+          <button
+            onClick={() => dispatch({ type: ACTION.ADDWORD, definition })}
+            className='bg-green-300 px-4 py-2 hover:bg-green-400'>Add {definition.word} to Lexicon</button>
+        </section>}
       <ListWords words={words}></ListWords>
-      {/* <pre>{JSON.stringify(words, null, 2)}</pre> */}
+      <pre>{JSON.stringify(words, null, 2)}</pre>
       {/* Make a collection of words */}
       {/* Add word to collection */}
       {/* Remove word from collection */}
       {/* Look up a word (and its synonyms, antonyms, etc.) */}
     </div>
   );
-}
-
-function getter() {
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com',
-      'X-RapidAPI-Key': '06678c9f50msh8e8dc3e7442f9f3p14404djsne386b9f689a7'
-    }
-  };
-
-  fetch('https://wordsapiv1.p.rapidapi.com/words/hatchback/typeOf', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
 }
 
 export default App;
