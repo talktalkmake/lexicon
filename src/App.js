f2e10cb6cf8d73a5a2c3e03c82539ab3e2531d8f
Tom Hart <kitsonian@gmail.com>
2022-05-10 15:40:39 -0400

    Adding showMore/less button for truncated defintiions

--- src/App.js
+++ src/App.js
@@ -35,9 +35,10 @@ const reducer = (state, action) => {
 
 function App() {
 
-  const [lexicon, dispatch] = useReducer(reducer, getLocalStorage() || [])
-  const [word, setWord] = useState('')
-  const [definition, setDefinition] = useState('')
+  const [lexicon, dispatch] = useReducer(reducer, getLocalStorage() || []);
+  const [word, setWord] = useState('');
+  const [definition, setDefinition] = useState('');
+  const [showMore, setShowMore] = useState(new Array(lexicon.length).fill(true));
 
   function getWordFromAPI() {
     const options = {
@@ -54,37 +55,63 @@ function App() {
       .catch(err => console.error(err));
   }
 
-  function ShowDefinitionList({ word, definitions }) {
+  const handleSetShowMore = i => {
+    const updatedSetShowMoreVal = [...showMore];
+    updatedSetShowMoreVal[i] = !showMore[i];
+    setShowMore(updatedSetShowMoreVal);
+  }
+
+  function ShowDefinitionList({ word, definitions }, i) {
+    const numberOfDefinitions = definitions.length;
+    const multipleDefinitions = numberOfDefinitions > 1;
+    const truncated = multipleDefinitions && showMore[i];
+    const inLexicon = isWordInLexicon(word, lexicon);
     return (<>
       <article
         key={word}
-        className='shadow bg-white rounded p-4 mb-10 sm:p-10 relative'>
-        {isWordInLexicon(word, lexicon)
+        className={`${truncated && `overflow-hidden max-h-64 `}shadow bg-white rounded p-4 pb-16 mb-10 sm:p-10 sm:pb-20 relative`}>
+        {inLexicon
           ? <button
             className='bg-red-600 text-white px-4 py-2 rounded-full absolute top-4 right-4 sm:top-10 sm:right-10'
             onClick={() => dispatch({ type: ACTION.REMOVEWORD, word })}>
-            remove
-          </button>
-          : <button
-            onClick={() => {
-              dispatch({ type: ACTION.ADDWORD, newWord: { word, definitions: [...definitions] } });
-              setDefinition('');
-            }
-            }
-            className='bg-green-300 hover:bg-green-400 px-4 py-2 rounded-full absolute top-0 right-0'>
-            Add {word} to Lexicon
-          </button>
+            remove</button>
+          :
+          <>
+            <button
+              onClick={() => {
+                dispatch({ type: ACTION.ADDWORD, newWord: { word, definitions: [...definitions] } });
+                setDefinition('');
+              }}
+              className='bg-green-300 hover:bg-green-400 px-4 py-2 rounded-full absolute top-4 right-4 sm:top-10 sm:right-10'>
+              Add to Lexicon</button>
+          </>
         }
 
         <header className='flex'>
           <h3 className='text-2xl font-semibold uppercase tracking-widest'>{word}</h3>
-          <p>{definitions.length}</p>
+          <p>{numberOfDefinitions}</p>
         </header>
         {definitions.map(definition =>
           <dl key={definition.definition}>
             <dt className='italic font-serif pt-5'>{definition.partOfSpeech}</dt>
             <dd className='text-xl'>{definition.definition}</dd>
-          </dl>)}
+          </dl>
+        )}
+        {truncated ?
+          <footer className='bg-gradient-to-t from-white absolute left-0 right-0 bottom-0 h-40'>
+            <button
+              onClick={() => handleSetShowMore(i)}
+              className='bg-gray-500 text-white px-4 py-2 rounded-full absolute bottom-4 left-1/2 -translate-x-1/2'
+            >Show more</button>
+          </footer>
+          :
+          <footer className='absolute left-0 right-0 bottom-0 h-40'>
+            <button
+              onClick={() => handleSetShowMore(i)}
+              className='bg-gray-100 px-4 py-2 rounded-full absolute bottom-4 left-1/2 -translate-x-1/2'
+            >Show less</button>
+          </footer>
+        }
       </article >
     </>
     );
@@ -96,7 +123,7 @@ function App() {
         <p className='mt-10 text-3xl'>Your lexicon has no words</p>
         <p className='mt-2 text-xl'>Search for a word at the top of the page.</p>
       </>
-      : lexicon.map(word => ShowDefinitionList(word))
+      : lexicon.map((word, i) => ShowDefinitionList(word, i))
   }
 
   return (
