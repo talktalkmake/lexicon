54d6a4d9a764abfc1be32457aee96c40a994fe75
Tom Hart <kitsonian@gmail.com>
2022-05-15 15:44:58 -0400

    adding adverbs

--- src/App.js
+++ src/App.js
@@ -183,6 +183,8 @@ const getPartOfSpeech = wordType => {
       return 'bg-emerald-100';
     case 'verb':
       return 'bg-indigo-100';
+    case 'adverb':
+      return 'bg-lime-100';
     case 'adjective':
       return 'bg-rose-100';
     default:
