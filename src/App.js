13ff50cbe9f23c511fea43e687ea02392dfb67d4
Tom Hart <kitsonian@gmail.com>
2022-05-15 14:27:36 -0400

    adding getPartOfSpeech()

--- src/App.js
+++ src/App.js
@@ -175,3 +175,16 @@ function App() {
 }
 
 export default App;
+
+const getPartOfSpeech = wordType => {
+  switch(wordType){
+    case 'noun':
+      return 'bg-emerald-100';
+    case 'verb':
+      return 'bg-indigo-100';
+    case 'adjective':
+      return 'bg-rose-100';
+    default:
+      return 'red';
+  }
+}
